6a24a8d3097af9bc09be02f55e698019e74eeb34
coffee-xzm <644334593@qq.com>
2025-07-28 21:33:19 +0800

    本地第一次初始化

--- src/rm_auto_aim/armor_detector/src/armor_detector.cpp
+++ src/rm_auto_aim/armor_detector/src/armor_detector.cpp
@@ -29,6 +29,9 @@
 #include <opencv2/core/types.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+
+#include <fmt/core.h> // <-- 添加这一行
+
 // project
 #include "armor_detector/types.hpp"
 #include "rm_utils/common.hpp"
