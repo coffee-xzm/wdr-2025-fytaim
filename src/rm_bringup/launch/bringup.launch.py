4876e8c708713004a4c53d470be72ee065813929
wdr <2834748575@qq.com>
2025-07-31 19:28:39 +0800

    能正常运行

--- src/rm_bringup/launch/bringup.launch.py
+++ src/rm_bringup/launch/bringup.launch.py
@@ -59,7 +59,7 @@ def generate_launch_description():
         #     extra_arguments=[{'use_intra_process_comms': True}]
         # )
         image_node  = ComposableNode(
-            package='rm_camera_driver',
+            package='hik_camera',
             plugin='hik_camera::HikCameraNode',
             name='camera_driver',
             parameters=[get_params('camera_driver')],
